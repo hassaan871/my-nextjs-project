4e88415fcb248a314e9f4eab342823de6d475a81
Hassaan <87420500+hassaan871@users.noreply.github.com>
2024-12-25 17:44:55 +0500

    useRouter

--- pages/portfolio/[placeholder].js
+++ pages/portfolio/[placeholder].js
@@ -1,6 +1,12 @@
-import React from 'react'
+import { useRouter } from 'next/router';
+import React from 'react';
 
 const PlaceHolder = () => {
+    const router = useRouter();
+
+    console.log( router.pathname );
+    console.log( router.query );
+    
   return (
     <div>
       <h1>This is a Portfolio Page</h1>
